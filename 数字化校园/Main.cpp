4cba0a0c9b48bc0e03a8322659f4c540da1495a1
liuzhijiang@LAPTOP-PBB5UROK <2636747115@qq.com>
2023-12-08 00:38:52 +0800

    部分代码补全

--- 数字化校园/Main.cpp
+++ 数字化校园/Main.cpp
@@ -3,6 +3,11 @@
 
 using namespace  std;
 
+map<string,int>name_id;
+map<int,string>id_name;
+
+
+
 void menu();
 void Init();
 
@@ -19,30 +24,50 @@ void menu()
 
 void Init()
 {
-
+    name_id["海豚自行车行"] = 1;
+    name_id["小西南门"] = 2;
+    name_id["华联超市"] = 3;
+    name_id["网络安全空间学院"] = 4;
+    name_id["网球场"] = 5;
+    name_id["青春广场"] = 6;
+    name_id["图书馆"] = 7;
+    name_id["一教A"] = 8;
+    name_id["南门"] = 9;
+    name_id["文科楼"] = 10;
+    id_name[1] = "海豚自行车行";
+    id_name[2] = "小西南门";
+    id_name[3] = "华联超市";
+    id_name[4] = "网络安全空间学院";
+    id_name[5] = "网球场";
+    id_name[6] = "青春广场";
+    id_name[7] = "图书馆";
+    id_name[8] = "一教A";
+    id_name[9] = "南门";
+    id_name[10] = "文科楼";
 }
 int main(int argc, char** argv)
 {
     Graph* G;
     FILE *fid;
 
-    if (argc != 2) {
-        cout << "Usage: grdijkl1 <file>\n";
-        exit(-1);
-    }
-
-    if ((fid = fopen(argv[1], "rt")) == NULL) {
-        cout << "Unable to open file |" << argv[1] << "|\n";
-        exit(-1);
-    }
+//    if (argc != 2) {
+//        cout << "Usage: grdijkl1 <file>\n";
+//        exit(-1);
+//    }
+//
+//    if ((fid = fopen(argv[1], "rt")) == NULL) {
+//        cout << "Unable to open file |" << argv[1] << "|\n";
+//        exit(-1);
+//    }
 
     Init();
     while(true){
         menu();
         int input;
-        cout<<"请选择你的操作:";
+        cout<<"请选择你的操作:" << endl;
         cin >> input;
         if(input == 1){
+            cout << "请输入您要查询的地点名称：" << endl;
 
         }else if(input == 2){
 
