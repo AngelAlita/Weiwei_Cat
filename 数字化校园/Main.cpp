2e767e01b2b2e64dc32a097a549cc5dd38ddf116
lee <1136142252@qq.com>
2023-12-08 00:57:44 +0800

    整合

--- 数字化校园/Main.cpp
+++ 数字化校园/Main.cpp
@@ -22,7 +22,21 @@ void menu()
     printf("3.退出\n");
 }
 
-void Init()
+//创建邻接表
+template<typename Graphl>
+Graphl* createGraph(FILE* fid){
+    int numVertex, numEdge;
+    fscanf(fid, "%d %d", &numVertex, &numEdge);
+    Graphl* G = new Graphl(numVertex);
+    int u, v, w;
+    while (fscanf(fid, "%d %d %d", &u, &v, &w) == 3) {
+        G->setEdge(u, v, w);
+    }
+    return G;
+}
+
+
+Graphl* Init(FILE* fid)
 {
     name_id["海豚自行车行"] = 1;
     name_id["小西南门"] = 2;
@@ -44,9 +58,13 @@ void Init()
     id_name[8] = "一教A";
     id_name[9] = "南门";
     id_name[10] = "文科楼";
+    Graphl* G;
+    G = createGraph<Graphl>(fid);
+    return G;
+
 }
 
-void jianjie(int id){
+void description(int id){
     if(id == 1) printf("1.海豚自行车行:川大校内唯一一家自行车铺，可进行修理车子、安装车子等");
     if(id == 2) printf("2.小西南门：出门即是文星地铁站，紧挨学生生活区，十分方便");
     if(id == 3) printf(" 3.华联超市：各种生活用品一应俱全，还拥有关东煮、蜜雪冰城等多家店铺,是学生们极其喜爱的地方" );
@@ -59,9 +77,36 @@ void jianjie(int id){
     if(id == 10) printf(" 10.文科楼：具有历史文化气息的教学楼，马克思学院、法学院等就在此处");
 }
 
+
+int minVertex(Graph* G, int* D) { // Find min cost vertex
+    int i, v = -1;
+    // Initialize v to some unvisited vertex
+    for (i=0; i<G->n(); i++)
+        if (G->getMark(i) == UNVISITED) { v = i; break; }
+    for (i++; i<G->n(); i++)  // Now find smallest D value
+        if ((G->getMark(i) == UNVISITED) && (D[i] < D[v]))
+            v = i;
+    return v;
+}
+
+// Compute shortest path distances from "s".
+// Return these distances in "D".
+void Dijkstra(Graph* G, int* D, int s) {
+    int i, v, w;
+    for (i=0; i<G->n(); i++) {      // Process the vertices
+        v = minVertex(G, D);
+        if (D[v] == INFINITY) return; // Unreachable vertices
+        G->setMark(v, VISITED);
+        for (w=G->first(v); w<G->n(); w = G->next(v,w))
+            if (D[w] > (D[v] + G->weight(v, w)))
+                D[w] = D[v] + G->weight(v, w);
+    }
+}
+
+
 int main(int argc, char** argv)
 {
-    Graph* G;
+    Graphl* G;
     FILE *fid;
 
     if (argc != 2) {
@@ -74,7 +119,13 @@ int main(int argc, char** argv)
         exit(-1);
     }
 
-    Init();
+    G = Init(fid);
+    if (G == NULL) {
+        cout << "Unable to create graph\n";
+        exit(-1);
+    }
+
+
     while(true){
         menu();
         int input;
@@ -87,9 +138,20 @@ int main(int argc, char** argv)
             cout << "序号：" << name_id[place_name] << endl;
             cout << "名称：" << place_name << endl;
             cout << "简介：" ;
-            jianjie(name_id[place_name]);
+            description(name_id[place_name]);
 
         }else if(input == 2){
+            int D[G->n()];
+            for (int i=0; i<G->n(); i++)     // Initialize
+                D[i] = INFINITY;
+            D[0] = 0;
+
+            int id1,id2;
+            cout << "你要查询的两个地点（序号）" ;
+            cin  >> id1 >> id2;
+            D[id1] = 0;
+            Dijkstra(G, D, 0);
+            cout << D[id2] << endl;
 
         }else if(input == 3){
             break;
